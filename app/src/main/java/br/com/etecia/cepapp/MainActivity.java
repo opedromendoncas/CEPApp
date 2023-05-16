c13f1865f2e75614c5fce074370b1c9371cabfe2
opedromendoncas <pedoca.360@gmail.com>
2023-05-16 20:20:06 -0300

    atualizacao

--- app/src/main/java/br/com/etecia/cepapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/cepapp/MainActivity.java
@@ -3,12 +3,48 @@ package br.com.etecia.cepapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnBuscaCep, btnCadastrar, btnAlterar, btnExcluir, btnLimpar;
+
+    EditText edtCEP;
+
+    TextView txtEndereco, txtNumero, txtBairro, txtCidade, txtComplemento;
+
+    Spinner spnrEstado;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnBuscaCep = findViewById(R.id.idBuscaCep);
+        btnCadastrar = findViewById(R.id.idCadastrar);
+        btnAlterar = findViewById(R.id.idAlterar);
+        btnExcluir = findViewById(R.id.idExcluir);
+        btnLimpar = findViewById(R.id.idLimpar);
+        edtCEP = findViewById(R.id.txtCep);
+        txtEndereco = findViewById(R.id.lblEndereco);
+        txtNumero = findViewById(R.id.lblNumero);
+        txtBairro = findViewById(R.id.idBairro);
+        txtCidade = findViewById(R.id.idCidade);
+        txtComplemento = findViewById(R.id.idComplemento);
+        spnrEstado = findViewById(R.id.idSpinner);
+
+        btnBuscaCep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
